a9e2c0910cc8e6289359dc7371afea06566b8002
Rahul Shakya <rahulshakya7685@gmail.com>
2025-11-02 20:55:35 +0530

    Post 12 or 13

---
+++ Post12/SmallestLetter.java
@@ -0,0 +1,22 @@
+package Post12;
+
+public class SmallestLetter {
+    public static void main(String[] args) {
+        char[] letters = { 'c', 'f', 'g' };
+        char target = 'i';
+        System.out.println(nextGreatestLetter(letters, target));
+    }
+    static char nextGreatestLetter(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < letters[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return letters[start % letters.length];
+    }
+}
