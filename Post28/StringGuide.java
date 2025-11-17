0a77c18dcb25f2d4477b9a9eabe43587af07e750
Rahul Shakya <rahulshakya7685@gmail.com>
2025-11-17 11:24:45 +0530

    Day 28 Of 100Day of DSA

---
+++ Post28/StringGuide.java
@@ -0,0 +1,10 @@
+package Post28;
+
+public class StringGuide {
+    public static void main(String[] args) {
+        String a = new String("Java");
+        String b = new String("Java");
+        System.out.println(a == b); // false (different heap objects)
+        System.out.println(a.equals(b)); // true
+    }
+}
