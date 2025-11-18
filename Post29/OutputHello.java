3557f46d9174db38e95c39081bbd3f346f25e69b
Rahul Shakya <rahulshakya7685@gmail.com>
2025-11-18 22:28:54 +0530

    Day 29 Of 100 Days of DSA

---
+++ Post29/OutputHello.java
@@ -0,0 +1,8 @@
+public class OutputHello {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println(new int[]{2, 3,4});
+        String name = null;
+        System.out.println(name);
+    }
+}
---
+++ Post29/PrettyPrinting.java
@@ -0,0 +1,5 @@
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        System.out.printf("Hello my name is %s and i am %s", "kunal", "cool");
+    }
+}
