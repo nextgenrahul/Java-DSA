2c7882dcf4629f3395f8473930183c2e795eafd1
Rahul Shakya <rahulshakya7685@gmail.com>
2025-12-19 21:31:31 +0530

    New Updates

--- Post37/Fibo.java
+++ Post37/Fibo.java
@@ -2,13 +2,17 @@ package Post37;
 
 public class Fibo {
     public static void main(String[] args) {
-        System.out.println(fibo(6));
+        int ans = print(50);
+        System.out.println(ans);
+
     }
 
-    static int fibo(int n){
+    static int print(int n) {
         if(n < 2){
             return n;
         }
-        return fibo(n - 1) + fibo(n - 2);
-    }   
+        return print(n-1)+print(n-2);
+    }
+
 }
+
